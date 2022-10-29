e62a99fd9f96f703dbb8604ccdc3a615dac19ee4
Cayman <caymannava@gmail.com>
2022-10-29 11:49:51 -0400

    fix: add tests for isIP* (#5)

--- src/is-ip.ts
+++ src/is-ip.ts
@@ -2,32 +2,17 @@ import { parseIP, parseIPv4, parseIPv6 } from "./parse.js";
 
 /** Check if `input` is IPv4. */
 export function isIPv4(input: string): boolean {
-  try {
-    parseIPv4(input);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  return Boolean(parseIPv4(input));
 }
 
 /** Check if `input` is IPv6. */
 export function isIPv6(input: string): boolean {
-  try {
-    parseIPv6(input);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  return Boolean(parseIPv6(input));
 }
 
 /** Check if `input` is IPv4 or IPv6. */
 export function isIP(input: string): boolean {
-  try {
-    parseIP(input);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  return Boolean(parseIP(input));
 }
 
 /**
